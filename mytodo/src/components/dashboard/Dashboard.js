002778895b4b36ddb8f72e4dcb83dbf6c667b712
Animesh Kar <karanimesh687@gmail.com>
2021-02-17 17:35:35 +0530

    hosting in firebasse

--- mytodo/src/components/dashboard/Dashboard.js
+++ mytodo/src/components/dashboard/Dashboard.js
@@ -40,6 +40,6 @@ const mapStateToProps = (state) => {
 export default compose(
     connect(mapStateToProps),
     firestoreConnect([
-        { collection: 'tasks' }
+        { collection: 'tasks'  }
     ])
 )(Dashboard)
